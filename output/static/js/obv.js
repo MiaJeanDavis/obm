3a378d95795d273b7981806ac7310562e67be20a
MiaJeanDavis <31977768+MiaJeanDavis@users.noreply.github.com>
2017-09-14 20:18:54 -0700

    Initial

---
+++ output/static/js/obv.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    /* Subscribed notification */
+    /* 
+    var notification = $.query.get('notification');
+    if (notification == "subscribed") {
+	$(".alert").show();
+	console.log("Subscription box closed!");
+	Cookies.set('subscription_box_closed', 'yes', { expires: 500 });
+	$(".alert").delay(3000).fadeOut();	
+    }
+    */
+
+    /* Set src cookie */
+    var source = $.query.get('src');
+    if (source) {
+	Cookies.set('src', source, { expires: 10 });	
+    } else {
+	/* If there's no src in url, try getting source from cookies */
+	source = Cookies.get('src');
+    }
+    if (source) {
+	$("#email-src").val(source);
+	$("#email-src-modal").val(source);
+	$("#email-src-newsletter").val(source);					
+    }
+
+    var modal = $.query.get('modal');
+    if (modal == "thankyou") {
+	$("#thankyou-modal").modal();
+    }
+    
+
+    /* Google Analytics */
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-44003603-25', 'auto');
+    ga('send', 'pageview');
+});
+
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+// var config = require('./config.json');
+
+app.set('view engine', 'ejs')
+
+/* Serve pelican-generated output files */
+app.use('/', express.static(path.resolve(__dirname, './output')))
+
+// Routes
+/*
+app.get('/', function (req, res) {
+    res.render('index', {html: 'Hello', config: config})
+})
+*/
+
+const port = process.env.PORT || 3000
+app.listen(port, function () {
+    console.log('Running on port 3000!')
+})
